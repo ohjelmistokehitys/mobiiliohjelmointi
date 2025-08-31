a445545439d41941cd276dfc739485b020f3b74a
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-08-31 20:37:57 +0300

    Updated hello world to use same styles as other screens

--- app/hello.tsx
+++ app/hello.tsx
@@ -1,33 +1,21 @@
+import Container from "@/components/Container";
+import MyButton from "@/components/MyButton";
+import MyInput from "@/components/MyInput";
+import Title from "@/components/Title";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Hello() {
-  const [temp, setTemp] = useState("");
-  const [name, setName] = useState("etunimi");
+    const [temp, setTemp] = useState("");
+    const [name, setName] = useState("stranger");
 
-  return <View style={styles.container}>
-    <Text>Hello {name}</Text>
-    <TextInput
-      value={temp}
-      style={styles.firstName}
-      onChangeText={text => setTemp(text)}
-    />
-    <Button title="Ok" onPress={() => setName(temp)} />
-  </View>;
+    return <Container>
+        <Title>{`Hello ${name}`}</Title>
+        <MyInput
+            value={temp}
+            placeholder="Enter your name"
+            onChangeText={text => setTemp(text)}
+        />
+        <MyButton onPress={() => setName(temp)}>Ok</MyButton>
+    </Container>;
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "silver",
-    padding: 10,
-    gap: 10,
-    justifyContent: "center",
-    flex: 1
-  },
-  firstName: {
-    borderColor: "black",
-    borderWidth: 1,
-    backgroundColor: "white"
-  }
-});
