5a5c2d054faa72e6e1d756fac752b604b8cdafb4
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-09-08 12:34:21 +0300

    Added TypeScript types for GitHub repository search response

---
+++ networking/repositorySearch.ts
@@ -0,0 +1,12 @@
+import { RepoSearchResponse } from "./githubTypes";
+
+
+export async function searchRepositories(keyword: string, per_page = 1): Promise<RepoSearchResponse> {
+    const response = await fetch(`https://api.github.com/search/repositories?q=${encodeURIComponent(keyword)}&per_page=${per_page}`);
+
+    if (!response.ok) {
+        throw new Error(`Received error ${response.status}`);
+    }
+    return await response.json();
+}
+
