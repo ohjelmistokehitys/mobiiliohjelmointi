fd0d1528855f9dc30a076b2be6260a209ce26c80
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2026-01-28 13:17:55 +0200

    Icons for a few tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
+import { Strong } from '@/components/ui/basic-components';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -21,6 +22,20 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="shoppingList"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color }) => <Strong>🛒</Strong>
+        }}
+      />
+      <Tabs.Screen
+        name="news"
+        options={{
+          title: "News",
+          tabBarIcon: ({ color }) => <Strong>📰</Strong>
         }}
       />
     </Tabs>
